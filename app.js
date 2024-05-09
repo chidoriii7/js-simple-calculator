93215538756e70e139bd657b57e9576c2dd006bd
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-09 22:51:16 +0200

    calculator - eventlisteners added

---
+++ app.js
@@ -0,0 +1,91 @@
+
+let result = document.getElementById('result-bar');
+
+//         //
+// NUMBERS //
+//         //
+
+let number0 = document.getElementById('number-0');
+number0.addEventListener('click', function(){
+    result.innerText = '0';
+    // console.log('ho cliccato il numero 0')
+})
+let number1 = document.getElementById('number-1');
+number1.addEventListener('click', function(){
+    result.innerText = '1';
+    // console.log('ho cliccato il numero 1')
+})
+let number2 = document.getElementById('number-2');
+number2.addEventListener('click', function(){
+    result.innerText = '2';
+    // console.log('ho cliccato il numero 2')
+})
+let number3 = document.getElementById('number-3');
+number3.addEventListener('click', function(){
+    result.innerText = '3';
+    // console.log('ho cliccato il numero 3')
+})
+let number4 = document.getElementById('number-4');
+number4.addEventListener('click', function(){
+    result.innerText = '4';
+    // console.log('ho cliccato il numero 4')
+})
+let number5 = document.getElementById('number-5');
+number5.addEventListener('click', function(){
+    result.innerText = '5';
+    // console.log('ho cliccato il numero 5')
+})
+let number6 = document.getElementById('number-6');
+number6.addEventListener('click', function(){
+    result.innerText = '6';
+    // console.log('ho cliccato il numero 6')
+})
+let number7 = document.getElementById('number-7');
+number7.addEventListener('click', function(){
+    result.innerText = '7';
+    // console.log('ho cliccato il numero 7')
+})
+let number8 = document.getElementById('number-8');
+number8.addEventListener('click', function(){
+    result.innerText = '8';
+    // console.log('ho cliccato il numero 8')
+})
+let number9 = document.getElementById('number-9');
+number9.addEventListener('click', function(){
+    result.innerText = '9';
+    // console.log('ho cliccato il numero 9')
+})
+
+//           //
+// OPERATORS //
+//           //
+
+let equal = document.getElementById('equal');
+equal.addEventListener('click', function(){
+    // console.log('ho cliccato =');
+})
+
+let reset = document.getElementById('reset');
+reset.addEventListener('click', function(){
+    // console.log('ho cliccato C');
+})
+
+let plus = document.getElementById('plus');
+plus.addEventListener('click', function(){
+    // console.log('ho cliccato +');
+})
+
+let minus = document.getElementById('minus');
+minus.addEventListener('click', function(){
+    // console.log('ho cliccato -');
+})
+
+let times = document.getElementById('times');
+times.addEventListener('click', function(){
+    // console.log('ho cliccato x');
+})
+
+let divided = document.getElementById('divided');
+divided.addEventListener('click', function(){
+    // console.log('ho cliccato il diviso');
+})
